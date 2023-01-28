35c199ef4e5a3e2800a64aeb091d0ff81adbd6c5
Muhammad Najib Bala <45116287+NAJIB-B@users.noreply.github.com>
2023-01-28 11:31:54 +0100

    fixed zero error

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const convert =(value)=>{
    console.log(answer.innerHTML, "from najib")
    for(let i =0; i < value.length; i++){
      
-       const html = `<p>${value[i]}--${value[i].charCodeAt(0).toString(2)}</p>`
-
+       if(value[i].charCodeAt(0).toString(2).length === 6){
+       const html = `<p>${value[i]}--0${value[i].charCodeAt(0).toString(2)}</p>`
        answer.insertAdjacentHTML("beforeend", html)
+    }else{
+      const html = `<p>${value[i]}--${value[i].charCodeAt(0).toString(2)}</p>`
+      answer.insertAdjacentHTML("beforeend", html)
+     }
    }
 
 }
